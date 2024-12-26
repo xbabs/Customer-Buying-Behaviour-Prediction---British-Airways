77e8fa22db427c1687031e36ecf2f169fe83be72
Toluwase Babalola <80902439+xbabs@users.noreply.github.com>
2024-12-26 14:40:43 +0000

    Update customer_booking_predictor.py

--- customer_booking_predictor.py
+++ customer_booking_predictor.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1zqVBDgLpQgNU_DCpqYwnRXH_MygHi3zE
 """
 
-pip install streamlit
+#pip install streamlit
 
 import pandas as pd
 import numpy as np
