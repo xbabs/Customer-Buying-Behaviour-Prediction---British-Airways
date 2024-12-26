29c8e78fc3d158befe3a87d67c8fd85fa4939075
Toluwase Babalola <80902439+xbabs@users.noreply.github.com>
2024-12-26 15:19:05 +0000

    Update customer_booking_predictor.py

--- customer_booking_predictor.py
+++ customer_booking_predictor.py
@@ -11,7 +11,7 @@ Original file is located at
 
 import pandas as pd
 import numpy as np
-#import seaborn as sns
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
